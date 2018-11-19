53bd1229cd656944bb30553a07f9aca681d148f6
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-19 10:27:47 -0500

    Finished Version 1

---
+++ goblinFight.py
@@ -0,0 +1,48 @@
+hero_name = raw_input("What is thy name, brave adventurer?  ")
+
+def fight():
+    print "Welcome %s. Thou art brave! " %hero_name
+    hero = {
+        "Health" : 10,
+        "Power" : 5
+    }
+    goblin = {
+        "Health" : 6,
+        "Power" : 2
+    }
+    print "Fight the Goblin!"
+ 
+    while (hero["Health"] > 0 and goblin["Health"] > 0):
+        message = """
+        You have %d health and %d power. 
+        The goblin has %d health and %d power.
+        What do you want to do?
+        1. fight goblin
+        2. get jiggy with it
+        3. flee
+        """ 
+
+        print message % (hero["Health"], hero["Power"], goblin["Health"], goblin["Power"])
+
+        user_input = raw_input()
+
+        if (user_input == "1"):
+            # The hero has decided to attack!
+            # Subtract goblins health by hero power
+            goblin["Health"] -= hero["Power"]
+            print "You have done %d damage to the goblin!" % hero["Power"]
+        elif (user_input == "2"):
+            hero["Health"] += 10
+            print """The goblin stares at you in disbelief of your stupidity!
+            His jaw drops as your wounds heal."""
+            print "Your health is now %d." % hero["Health"]
+        elif (user_input == "3"):
+            print "ALRIGHT %s, SEE YA NERDDDDDDDD" % hero_name.upper()
+            break
+        else:
+            hero["Health"] -= goblin["Power"]
+            print "YOU FOOL! You have stumbledith (invalid input)"
+            print "The goblin attacked you for %d" % goblin["Power"]
+            print "Your health is now %d" % hero["Health"]
+
+fight()
