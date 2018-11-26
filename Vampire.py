ab39c8642e12e31c0c32ac6f250b7834fc9e2d1e
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-26 13:52:12 -0500

    Fixed simple typo

--- Vampire.py
+++ Vampire.py
@@ -2,7 +2,7 @@
 from Character import Character
 
 # this is a sub clsss of character
-class Vampire(object):
+class Vampire(Character):
     def __init__(self):
         # import super class with parameters
         super(Vampire, self).__init__("Vampire", 15 , 4)
